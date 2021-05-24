d7965e2f710af9ee197a57fac1ec7698cd329783
Ciensprog <jorg.leonrd@gmail.com>
2021-05-23 23:53:46 -0600

    Export core functionalities

--- src/index.ts
+++ src/index.ts
@@ -7,20 +7,10 @@
 
 export * from './interfaces/core/Khur';
 export * from './interfaces/core/Register';
+export * from './interfaces/core/Request';
+export * from './interfaces/handlers/Command';
 export * from './interfaces/structures/Command';
 export * from './interfaces/structures/Event';
-
-/*
-|--------------------------------------------------------------------------
-| Core
-|--------------------------------------------------------------------------
-*/
-
-export * from './core/Bot';
-export * from './core/Commands';
-export * from './core/Khur';
-export * from './core/Events';
-export * from './core/Register';
 
 /*
 |--------------------------------------------------------------------------
@@ -30,6 +20,35 @@ export * from './core/Register';
 
 export * from './structures/BaseCommand';
 export * from './structures/BaseEvent';
+
+/*
+|--------------------------------------------------------------------------
+| Core
+|--------------------------------------------------------------------------
+*/
+
+export * from './core/Bot';
+export * from './core/Commands';
+export * from './core/Events';
+export * from './core/Khur';
+export * from './core/Register';
+export * from './core/Request';
+
+/*
+|--------------------------------------------------------------------------
+| Handlers
+|--------------------------------------------------------------------------
+*/
+
+export * from './handlers/CommandHandler';
+
+/*
+|--------------------------------------------------------------------------
+| Extra
+|--------------------------------------------------------------------------
+*/
+
+export * from './utils/info';
 
 /*
 |--------------------------------------------------------------------------
