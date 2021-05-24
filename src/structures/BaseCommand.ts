2afd3f06bd89567ac084f5282d322620fbc4188b
Ciensprog <jorg.leonrd@gmail.com>
2021-05-24 12:46:26 -0600

    Include methods to delete command use

--- src/structures/BaseCommand.ts
+++ src/structures/BaseCommand.ts
@@ -1,5 +1,6 @@
 
-import { Message } from 'discord.js';
+import { GuildMember, Message } from 'discord.js';
+import { isEmpty } from 'lodash';
 
 import { CommandConfig, CommandStructure } from '../interfaces/structures/Command';
 
@@ -54,5 +55,37 @@ export class BaseCommand implements CommandStructure {
    */
   public async handle(): Promise<void> {
     throw new Error('BaseCommand -> handle() method is not defined');
+  }
+
+  /**
+   * Delete command use
+   */
+  public deleteCommandUse(): void {
+    const isDM: boolean = this.message.channel.type === 'dm';
+
+    if (!isDM) {
+      BaseCommand.checkAndTryDeleteCommandUse(this.message);
+    }
+  }
+
+  /**
+   * [Base] Delete command use
+   * @param message Message
+   */
+  public static checkAndTryDeleteCommandUse(message?: Message): void {
+    if (!isEmpty(message)) {
+      let botMember: GuildMember | null | undefined = message?.guild?.me;
+
+      if (!isEmpty(botMember)) {
+        botMember = <GuildMember> botMember;
+        const canIDeleteMessages: boolean = (
+          botMember.hasPermission('MANAGE_MESSAGES')
+        );
+
+        if (canIDeleteMessages) {
+          message?.delete().catch((): void => {});
+        }
+      }
+    }
   }
 }
