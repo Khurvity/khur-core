1cfef9db9430fb449903f83ef52fb3111dbf8659
Ciensprog <jorg.leonrd@gmail.com>
2021-05-23 23:44:48 -0600

    Add new class | Request

---
+++ src/interfaces/core/Request.ts
@@ -0,0 +1,53 @@
+
+/*
+|--------------------------------------------------------------------------
+| Interfaces: Core/Request
+|--------------------------------------------------------------------------
+|
+| ...
+|
+*/
+
+export interface RequestData {
+  prefix: string;
+  command: string;
+  content: string;
+  mentions: RequestMentions;
+  params: RequestParams;
+  raw: {
+    content: string;
+  };
+}
+
+export interface RequestMentions {
+  channels: Array<string>;
+  emotes: Array<RequestMentionsEmote>;
+  roles: Array<string>;
+  users: Array<string>;
+}
+
+export interface RequestMentionsEmote {
+  id: string;
+  animated: boolean;
+  name: string;
+  raw: string;
+}
+
+export interface RequestParams {
+  [key: string]: RequestParamsData;
+}
+
+export interface RequestParamsData {
+  content: string;
+  chunks: Array<string>;
+  mentions: RequestMentions;
+}
+
+export interface RequestValidations {
+  startsWith: {
+    channelMention: boolean;
+    id: boolean;
+    memberMention: boolean;
+    roleMention: boolean;
+  };
+}
